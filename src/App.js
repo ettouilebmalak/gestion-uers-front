d0c6ba63281ff6ab478094d85875cff01537c63b
malak <malak@gmail.com>
2026-02-24 18:27:03 +0100

    comments

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import AdminCentral from './AdminCentral';
 import AdminLocal from './AdminLocal';
 
 function App() {
-
+  //verifier if kecloack a charge le token d'user  sinon affiche chargement
   if (!keycloak.tokenParsed) {
     return <h1>Chargement...</h1>;
   }
 
+  //recuperer les roles a partir des tokens de keycloack
   const roles = keycloak.tokenParsed.realm_access.roles;
 
   if (roles.includes("admin-central")) {
   return <AdminCentral keycloak={keycloak} />;
 }
-
+//si le rrole est admin local afficher son composant
 if (roles.includes("admin-local")) {
   return <AdminLocal keycloak={keycloak} />;
 }return (
+  // sinon affihcer msg 
   <div style={{ textAlign: "center", marginTop: "100px", width:"100%" }}>
     <h1 >Accès refusé</h1>
-    
+    {/*button de deconnexion revenir au login*/}
     <button
       onClick={() =>
         keycloak.logout({
